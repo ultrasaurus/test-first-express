255661ea02687ad92ef47370cbba99806cea08ad
Sarah Allen <sarah.allen@gsa.gov>
2014-08-13 22:09:42 -0700

    test passes

--- test/test.js
+++ test/test.js
@@ -1,11 +1,10 @@
 var request = require('supertest')
   , express = require('express');
 
-var app = express();
+var app = require('../app');
 
 describe('Home page', function() {
   it("renders successfully", function(done) {
-    console.log('========= hey!');
     request(app).get('/').expect(200, done);    
   })
 })
